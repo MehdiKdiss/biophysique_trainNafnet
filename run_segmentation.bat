7b5e47037a3fb43a7dabc9518b577be4e64aa9e4
Mehdi Kdiss <mehdikdiss@gmail.com>
2026-05-18 20:07:47 +0100

    Delete run_segmentation.bat

--- run_segmentation.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Lancement de la segmentation avec l'environnement Python correct...
-"C:\Users\DELL\Desktop\version 1_12 module\python3\Scripts\python.exe" "c:\Users\DELL\Desktop\projet module\biophysique_trainNafnet\segmentation.py"
-pause
